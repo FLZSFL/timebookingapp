43b37e5f54808d85016ccc7e0a4cd615af68d734
Frenk Locmelis <frenk@flzssolutions.com>
2020-10-14 21:26:18 +0200

    test

--- src/main/java/com/flzssolutionsgmbh/projecttimebookingapp/ProjecttimebookingappApplication.java
+++ src/main/java/com/flzssolutionsgmbh/projecttimebookingapp/ProjecttimebookingappApplication.java
@@ -3,7 +3,7 @@ package com.flzssolutionsgmbh.projecttimebookingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*test*/
+
 @SpringBootApplication
 public class ProjecttimebookingappApplication {
 
