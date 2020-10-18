c87ac6652c637bf3473d3a69a68738a7447a6934
Frenk Locmelis <frenk@flzssolutions.com>
2020-10-18 18:53:47 +0200

    Reworking the structure according to FHNW suggestions

--- src/main/java/com/flzssolutionsgmbh/projecttimebookingapp/data/domain/User.java
+++ src/main/java/com/flzssolutionsgmbh/projecttimebookingapp/data/domain/User.java
@@ -57,5 +57,22 @@ public class User {
     public void setLn(String ln) {
         this.ln = ln;
     }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 }
 
---
+++ src/main/java/com/flzssolutionsgmbh/projecttimebookingapp/service/ProjectService.java
@@ -0,0 +1,31 @@
+package com.flzssolutionsgmbh.projecttimebookingapp.service;
+
+import com.flzssolutionsgmbh.projecttimebookingapp.data.domain.Project;
+import com.flzssolutionsgmbh.projecttimebookingapp.data.domain.User;
+import com.flzssolutionsgmbh.projecttimebookingapp.data.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    private String variable;
+
+
+    public void addProject(Project project, User user){
+        project.setUser(user);
+        projectRepository.save(project);
+    }
+
+    public List<Project> findUserProject(User user){
+
+        return projectRepository.findByUser(user);
+    }
+
+}
+
